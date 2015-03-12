d71e5551e19bf4340db4f6cbd0cc06a1db9ef22e
Daniel Sellers <daniel@designfrontier.net>
2015-03-11 19:53:22 -0600

    thought better of that decission and added av ersion to the api routes

--- routes/api.js
+++ routes/api.js
@@ -7,26 +7,26 @@ events.on('route:/api:get', function (connection) {
 	connection.res.end('route /api now responding to get requests');
 });
 
-events.on('route:/api/:app:get', function (connection) {
+events.on('route:/api/v1/:app:get', function (connection) {
 	'use strict';
 	
-	connection.res.end('route /api/:app now responding to get requests');
+	connection.res.end('route /api/v1/:app now responding to get requests');
 });
 
-events.on('route:/api/:app:post', function (connection) {
+events.on('route:/api/v1/:app:post', function (connection) {
 	'use strict';
 	
-	connection.res.end('route /api/:app now responding to post requests');
+	connection.res.end('route /api/v1/:app now responding to post requests');
 });
 
-events.on('route:/api/:app/:id:get', function (connection) {
+events.on('route:/api/v1/:app/:id:get', function (connection) {
 	'use strict';
 	
-	connection.res.end('route /api/:app/:id now responding to get requests');
+	connection.res.end('route /api/v1/:app/:id now responding to get requests');
 });
 
-events.on('route:/api/:app/:id:put', function (connection) {
+events.on('route:/api/v1/:app/:id:put', function (connection) {
 	'use strict';
 	
-	connection.res.end('route /api/:app/:id now responding to put requests');
+	connection.res.end('route /api/v1/:app/:id now responding to put requests');
 });
