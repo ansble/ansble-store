ab38affd90600f0a1fe8a3614eda8d59d1aca0e7
Daniel Sellers <daniel@designfrontier.net>
2015-03-11 21:30:30 -0600

    working on setting up the first routes handler

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,15 @@ events.on('route:/api:get', function (connection) {
 events.on('route:/api/v1/:app:get', function (connection) {
 	'use strict';
 	
-	connection.res.send('route /api/v1/:app now responding to get requests');
+	events.once('data:set:all:' + connection.params.app, function (data) {
+		if(data === null){
+			data = [];
+		}
+		
+		connection.res.send(data);
+	});
+
+	events.emit('data:get:all', {key: connection.params.key});
 });
 
 events.on('route:/api/v1/:app:post', function (connection) {
