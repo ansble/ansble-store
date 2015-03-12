952eff93358185042896e6c8ae6c3a90327bd595
Daniel Sellers <daniel@designfrontier.net>
2015-03-11 19:51:58 -0600

    removed ould routes form the start... and scaffolded out all the routes that we will need to make this work

--- routes/api.js
+++ routes/api.js
@@ -4,52 +4,29 @@ var events = require('monument').events;
 events.on('route:/api:get', function (connection) {
 	'use strict';
 	
-	connection.res.end('Welcome to the designfrontier.net api!');
+	connection.res.end('route /api now responding to get requests');
 });
 
-events.on('route:/api/articles:get', function (connection) {
-	'use strict';
-
-	if(typeof connection.query.since !== 'undefined'){
-		events.once('data:set:articles:since', function (articles) {
-			connection.res.send(articles);
-		});
-
-		events.emit('data:get:articles:since', connection.query.since);
-	} else {
-		events.once('data:set:articles', function (articles) {
-			connection.res.send(articles);
-		});
-
-		events.emit('data:get:articles');
-	}
-
-});
-
-events.on('route:/api/articles/:id:get', function (connection) {
+events.on('route:/api/:app:get', function (connection) {
 	'use strict';
 	
-	events.once('data:set:article:' + connection.params.id, function (article) {
-		if(typeof article !== 'undefined'){
-			connection.res.send(article);
-		} else {
-			events.emit('error:404', connection);
-		}
-	});
-
-	events.emit('data:get:articles', connection.params.id);
+	connection.res.end('route /api/:app now responding to get requests');
 });
 
-events.on('route:/api/search:get', function (connection) {
+events.on('route:/api/:app:post', function (connection) {
 	'use strict';
 	
-	if(typeof connection.query.q === 'undefined' && connection.query.q !== ''){
-		connection.res.end('You must include a queryparam q for this to work.');
-	} else {
-		events.once('articles:search:results', function (results) {
-			connection.res.send(results);
-		});
-		
-		events.emit('articles:search', connection.query.q);
-	}
+	connection.res.end('route /api/:app now responding to post requests');
+});
+
+events.on('route:/api/:app/:id:get', function (connection) {
+	'use strict';
+	
+	connection.res.end('route /api/:app/:id now responding to get requests');
+});
+
+events.on('route:/api/:app/:id:put', function (connection) {
+	'use strict';
+	
+	connection.res.end('route /api/:app/:id now responding to put requests');
 });
--- routes/stuff.js
+++
@@ -1,8 +0,0 @@
-var events = require('monument').events;
-
-
-events.on('route:/stuff:get', function (connection) {
-	'use strict';
-	
-	connection.res.end('route /stuff now responding to get requests');
-});
