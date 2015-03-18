10a0094f47beb4310d7625741eddb8956031ca97
Daniel Sellers <daniel@designfrontier.net>
2015-03-17 20:36:38 -0600

    added a gitignore for the mongo db that is in local dev.

--- security.js
+++ security.js
@@ -4,6 +4,7 @@ var jwt = require('jsonwebtoken')
 	, schema = require('./data/schemas')
 	, salt = 'it was the best of times it was the worst of times. It was the age of reason...'
 	, createJTI = require('./utils').generateID
+	, mongo = require('mongodb')
 	, key;
 
 //import the public key for this environment
