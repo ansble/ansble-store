f021791073b574dda229608b6c668cb7ace06629
Daniel Sellers <daniel@designfrontier.net>
2015-12-14 20:54:00 -0700

    added the new token for loader.io

--- routes/loaderio-a965750d0b14924648d4a814f35c567a.js
+++ routes/loaderio-a965750d0b14924648d4a814f35c567a.js
@@ -2,5 +2,5 @@
 const events = require('monument').events;
 
 events.on('route:/loaderio-a965750d0b14924648d4a814f35c567a:get', (connection) => {
-    connection.res.send('loaderio-a965750d0b14924648d4a814f35c567a');
+    connection.res.send('loaderio-1b3b503e8a877429e0f6cbd83baf0475');
 });
--- routes/loaderio-a965750d0b14924648d4a814f35c567a.test.js
+++ routes/loaderio-a965750d0b14924648d4a814f35c567a.test.js
@@ -13,10 +13,9 @@ describe('/loaderio-a965750d0b14924648d4a814f35c567a route file tests', () => {
     });
 
     it('should respond to route:/loaderio-a965750d0b14924648d4a814f35c567a:get', () => {
-        const response = 'loaderio-a965750d0b14924648d4a814f35c567a';
+        const response = 'loaderio-1b3b503e8a877429e0f6cbd83baf0475';
 
         events.emit('route:/loaderio-a965750d0b14924648d4a814f35c567a:get', fakeConnection);
-
         assert.strictEqual(fakeConnection.out().response, response);
     });
 });
